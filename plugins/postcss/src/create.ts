6f8017f0165ce06c199ac1ca9aebbde7dcc1818d
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-02-22 23:40:31 +0800

    feat: make postcss plugin extendable

---
+++ plugins/postcss/src/create.ts
@@ -0,0 +1,95 @@
+import type { Runtime } from "@ammolite/runtime";
+import type { PluginCreator, Root } from "postcss";
+import type { Format, Partial } from "ts-vista";
+
+import type { PluginOptions } from "#/@types/options";
+
+import * as Fsp from "node:fs/promises";
+
+import { createRuntime } from "@ammolite/runtime";
+
+import { getPaths } from "#/functions/path";
+import { name } from "../package.json";
+
+type CompleteCreatePluginOptions = {
+    name: string;
+    runtime: Runtime;
+};
+
+type CreatePluginOptions = Format<Partial<CompleteCreatePluginOptions>>;
+
+const createPlugin = (
+    createOptions?: CreatePluginOptions,
+): PluginCreator<PluginOptions> => {
+    const plugin: PluginCreator<PluginOptions> = (options?: PluginOptions) => {
+        const cwd: string = options?.cwd ?? process.cwd();
+
+        const include: readonly string[] =
+            options?.input?.include && (options.input.include.length ?? 0) > 0
+                ? options.input.include
+                : [
+                      "./src",
+                  ];
+
+        const exclude: readonly string[] =
+            options?.input?.exclude && (options.input.exclude.length ?? 0) > 0
+                ? options.input.exclude
+                : [];
+
+        const runtime: Runtime =
+            createOptions?.runtime ??
+            createRuntime({
+                cwd,
+                include,
+                exclude,
+            });
+
+        return {
+            postcssPlugin: createOptions?.name ?? name,
+            async Once(root: Root, { postcss, result }): Promise<void> {
+                const emit: boolean = options?.emit ?? true;
+
+                if (!emit) return void 0;
+
+                const included: string[] = await getPaths({
+                    cwd,
+                    paths: include ?? [
+                        "./src",
+                    ],
+                });
+
+                for await (const file of included) {
+                    // compile
+                    await runtime.compile({
+                        file,
+                        code: await Fsp.readFile(file, "utf-8"),
+                    });
+
+                    // add dependency
+                    result.messages.push({
+                        type: "dependency",
+                        plugin: name,
+                        file,
+                        parent: "ammolite.css",
+                    });
+                }
+
+                // append result
+                root.append(
+                    postcss.parse(await runtime.getCSS(), {
+                        from: "ammolite.css",
+                    }),
+                );
+            },
+        };
+    };
+
+    plugin.postcss = true;
+
+    return plugin;
+};
+
+type Plugin = ReturnType<typeof createPlugin>;
+
+export type { CreatePluginOptions, Plugin };
+export { createPlugin };
--- plugins/postcss/src/index.ts
+++ plugins/postcss/src/index.ts
@@ -1,65 +1,9 @@
-import type { Runtime } from "@ammolite/runtime";
-import type { PluginCreator, Root } from "postcss";
-
 import type { InputOptions, PluginOptions } from "#/@types/options";
+import type { Plugin } from "#/create";
 
-import * as Fsp from "node:fs/promises";
+import { createPlugin } from "#/create";
 
-import { createRuntime } from "@ammolite/runtime";
-
-import { getPaths } from "#/functions/path";
-import { name } from "../package.json";
-
-const plugin: PluginCreator<PluginOptions> = (options?: PluginOptions) => {
-    return {
-        postcssPlugin: name,
-        async Once(root: Root, { postcss, result }): Promise<void> {
-            const emit: boolean = options?.emit ?? true;
-
-            if (!emit) return void 0;
-
-            const cwd: string = options?.cwd ?? process.cwd();
-
-            const runtime: Runtime = createRuntime({
-                cwd,
-                include: options?.input?.include,
-                exclude: options?.input?.exclude,
-            });
-
-            const include: string[] = await getPaths({
-                cwd,
-                paths: options?.input?.include ?? [
-                    "./src",
-                ],
-            });
-
-            for await (const file of include) {
-                // compile
-                await runtime.compile({
-                    file,
-                    code: await Fsp.readFile(file, "utf-8"),
-                });
-
-                // add dependency
-                result.messages.push({
-                    type: "dependency",
-                    plugin: name,
-                    file,
-                    parent: "ammolite.css",
-                });
-            }
-
-            // append result
-            root.append(
-                postcss.parse(await runtime.getCSS(), {
-                    from: "ammolite.css",
-                }),
-            );
-        },
-    };
-};
-
-plugin.postcss = true;
+const plugin: Plugin = createPlugin();
 
 export default plugin;
 export type { InputOptions, PluginOptions };
